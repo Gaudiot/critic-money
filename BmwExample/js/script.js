d0a81e0cd1eeabbff2d0b58066ea88267e11c208
Victor Edmond Freire Gaudiot <vefg@WINDOWS.CIN.UFPE.BR>
2023-09-13 13:12:08 -0300

    adjust html file to support balance and dependents

--- BmwExample/js/script.js
+++ BmwExample/js/script.js
@@ -2,11 +2,10 @@ var module = angular.module('phone', ['xeditable'])
   .controller('pCtrl', function($scope) {
 
     bms.observe("formula", {
-      formulas: ["db", "active"],
+      formulas: ["CLIENTS"],
       translate: true,
       trigger: function(values) {
-        $scope.db = values[0];
-        $scope.active = values[1];
+        $scope.clients = values[0];
       }
     });
 
